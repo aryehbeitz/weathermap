cb8382dea7911bcd5e01e02a279184751e33c84d
Aryeh Beitz <aryehbeitz@gmail.com>
2025-05-26 06:31:08 +0300

    Add ESLint workflow and config

--- tests/version.test.js
+++ tests/version.test.js
@@ -1,11 +1,11 @@
-const request = require('supertest');
-const app = require('../server');
+const request = require("supertest");
+const app = require("../server");
 
-describe('GET /version.json', () => {
-  it('returns the version information', async () => {
-    const res = await request(app).get('/version.json');
+describe("GET /version.json", () => {
+  it("returns the version information", async () => {
+    const res = await request(app).get("/version.json");
     expect(res.statusCode).toBe(200);
-    expect(res.headers['content-type']).toMatch(/application\/json/);
-    expect(res.body).toHaveProperty('version');
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    expect(res.body).toHaveProperty("version");
   });
 });
